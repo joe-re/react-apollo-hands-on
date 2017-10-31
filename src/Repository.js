8784f2fac5b5af3a03b4fdbd4bc0cbd16da0b879
joe-re <joe.tialtngo@gmail.com>
2017-10-31 17:44:59 +0900

    add/remove star

--- src/Repository.js
+++ src/Repository.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import ADD_STAR_MUTATION from './graphql/AddStar';
+import REMOVE_STAR_MUTATION from './graphql/RemoveStar';
+import { graphql, compose } from 'react-apollo';
 
-export default function Repository(props) {
-  const { repository } = props;
+function Repository(props) {
+  const { addStar, removeStar, repository } = props;
   return (
-    <div className="repository">
+    <div className="repository" style={{margin: '16px'}}>
       <div>{repository.owner.login}/{repository.name}</div>
+      {repository.viewerHasStarred ?
+        <button onClick={() => removeStar(repository.id)}>remove star</button> :
+        <button onClick={() => addStar(repository.id)}>add star</button>
+      }
     </div>
   );
 }
+
+const RepositoryWithMutations = compose(
+  graphql(ADD_STAR_MUTATION, {
+    props: ({ ownProps, mutate }) => ({
+      addStar: (id: string) =>
+        mutate({ variables: { input: { starrableId: id } }, })
+    })
+  }),
+  graphql(REMOVE_STAR_MUTATION, {
+    props: ({ ownProps, mutate }) => ({
+      removeStar: (id: string) =>
+        mutate({ variables: { input: { starrableId: id } }, })
+    })
+  })
+)(Repository);
+
+export default RepositoryWithMutations;
